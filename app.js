246c8b6c159ea0ceb1e171b382cbe56f8fd38019
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-07-30 03:34:31 -0300

    delay

--- app.js
+++ app.js
@@ -44,7 +44,9 @@ app.get('/:text', (req, res) => {
                 message: 'Internal error.'
             });
         }
-        return res.status(200).json(data);
+        // setTimeout(() => {
+            return res.status(200).json(data)
+        // }, 2000);
     });
 })
 
